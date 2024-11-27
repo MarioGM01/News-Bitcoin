3045060a5147684323b958817d5bc79fde8b3a9d
MarioGM01 <mariogm1908@gmail.com>
2024-11-27 20:41:41 +0100

    prueba 3 Cors

--- src/components/noticia.jsx
+++ src/components/noticia.jsx
@@ -12,11 +12,17 @@ export const Noticia = () => {
         const fetchData = async () => {
             try {
                 setLoading(true);
-                
-                const response = await fetch('https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=bitcoin&apiKey=06f607ab823343b682313f468c2bdc7b');
+
+                // Proxy de CORS Anywhere
+                const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+                const apiUrl = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=06f607ab823343b682313f468c2bdc7b';
+
+                const response = await fetch(proxyUrl + apiUrl);
+
                 if (!response.ok) {
                     throw new Error('Error en la solicitud');
                 }
+
                 const result = await response.json();
                 setData(result.articles); // Guarda solo el array de artículos en data
             } catch (error) {
@@ -26,6 +32,7 @@ export const Noticia = () => {
             }
         };
 
+        // Asegúrate de habilitar CORS Anywhere antes de hacer la solicitud.
         fetchData();
     }, []);
 
@@ -34,36 +41,45 @@ export const Noticia = () => {
 
     return (
         <div className="container">
-
             <Row className="justify-content-center">
-                {data && data.map((article, index) => (
-                    // Verifica si article.urlToImage o article.title son nulos o tienen el valor "Removed"
-                    (article.urlToImage && article.title !== "Removed") ? (
-                        
-                        <Col key={article.url} xs={12} md={6} lg={4} className="mb-4">
-                            <Card id='card-sz'>
-                                <Card.Img variant="top" src={article.urlToImage} alt={article.title} id='card-img'/>
-                                <Card.Body>
-                                    <Card.Title>{article.title}</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">
-                                        Fuente: {article.source.name}
-                                    </Card.Subtitle>
-                                    <Card.Text>{article.description}</Card.Text>
-                                    <a href={article.url} target="_blank" rel="noopener noreferrer">
-                                        Leer más
-                                    </a>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small className="text-muted">Publicado el {new Date(article.publishedAt).toLocaleDateString()}</small>
-                                </Card.Footer>
-                            </Card>
-                        </Col>
-                    ) : (
-                        <></> // Si no cumple con las condiciones, no renderiza nada
-                    )
-                ))}
-
-
+                {data &&
+                    data.map((article, index) => (
+                        // Verifica si article.urlToImage o article.title son nulos o tienen el valor "Removed"
+                        article.urlToImage && article.title !== 'Removed' ? (
+                            <Col key={article.url} xs={12} md={6} lg={4} className="mb-4">
+                                <Card id="card-sz">
+                                    <Card.Img
+                                        variant="top"
+                                        src={article.urlToImage}
+                                        alt={article.title}
+                                        id="card-img"
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>{article.title}</Card.Title>
+                                        <Card.Subtitle className="mb-2 text-muted">
+                                            Fuente: {article.source.name}
+                                        </Card.Subtitle>
+                                        <Card.Text>{article.description}</Card.Text>
+                                        <a
+                                            href={article.url}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            Leer más
+                                        </a>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <small className="text-muted">
+                                            Publicado el{' '}
+                                            {new Date(article.publishedAt).toLocaleDateString()}
+                                        </small>
+                                    </Card.Footer>
+                                </Card>
+                            </Col>
+                        ) : (
+                            <></> // Si no cumple con las condiciones, no renderiza nada
+                        )
+                    ))}
             </Row>
         </div>
     );
